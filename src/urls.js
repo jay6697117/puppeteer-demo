10a04fbb7843b1b16eaf3a1495f7263ee1511c30
steve zhang <871506263@qq.com>
2021-01-09 11:59:44 +0800

    提交代码于: 2021-01-09 11:59:44

---
+++ src/urls.js
@@ -0,0 +1,29 @@
+const urls = [
+  { link: 'https://weread.qq.com/web/reader/cc0329f0720867eecc0f00akc81322c012c81e728d9d180' },
+  {
+    link: 'https://weread.qq.com/web/reader/cc0329f0720867eecc0f00akecc32f3013eccbc87e4b62e'
+  },
+  {
+    link: 'https://weread.qq.com/web/reader/cc0329f0720867eecc0f00aka87322c014a87ff679a21ea'
+  },
+  {
+    link: 'https://weread.qq.com/web/reader/cc0329f0720867eecc0f00ake4d32d5015e4da3b7fbb1fa'
+  },
+  {
+    link: 'https://weread.qq.com/web/reader/cc0329f0720867eecc0f00ak16732dc0161679091c5aeb1'
+  },
+  {
+    link: 'https://weread.qq.com/web/reader/cc0329f0720867eecc0f00ak8f132430178f14e45fce0f7'
+  },
+  {
+    link: 'https://weread.qq.com/web/reader/cc0329f0720867eecc0f00akc9f326d018c9f0f895fb5e4'
+  },
+  {
+    link: 'https://weread.qq.com/web/reader/cc0329f0720867eecc0f00ak45c322601945c48cce2e120'
+  },
+  {
+    link: 'https://weread.qq.com/web/reader/cc0329f0720867eecc0f00akd3d322001ad3d9446802347'
+  }
+];
+
+module.exports = urls;
--- src/weread.js
+++ src/weread.js
@@ -1,36 +1,26 @@
 const puppeteer = require('puppeteer');
 const {timeout} = require('../tools/tools.js');
+const urls = require('./urls.js')
 
 puppeteer.launch().then(async browser => {
   let page = await browser.newPage();
 
-  await page.goto('http://es6.ruanyifeng.com/#README');
+  await page.goto(urls[0].link);
   await timeout(2000);
-
-  let aTags = await page.evaluate(() => {
-    let as = [...document.querySelectorAll('ol li a')];
-    return as.map(a => {
-      return {
-        href: a.href.trim(),
-        name: a.text
-      };
-    });
-  });
-
-  await page.pdf({ path: `./data/weread/${aTags[0].name}.pdf` });
+  await page.pdf({ path: './data/weread/0.pdf'});
   page.close();
 
   // 这里也可以使用promise all，但cpu可能吃紧，谨慎操作
-  for (var i = 1; i < aTags.length; i++) {
+  for (let index = 1; index < urls.length; index++) {
     page = await browser.newPage();
 
-    var a = aTags[i];
+    let url = urls[index];
 
-    await page.goto(a.href);
+    await page.goto(url.link);
 
     await timeout(2000);
 
-    await page.pdf({ path: `./data/weread/${a.name}.pdf` });
+    await page.pdf({ path: `./data/weread/${index}.pdf` });
 
     page.close();
   }
